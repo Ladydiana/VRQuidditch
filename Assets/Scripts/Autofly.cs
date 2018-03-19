181eb76b0726926924c1a6368e46c006e6a48de0
Ladydiana <diana.culincu@gmail.com>
2018-03-19 19:16:44 +0200

    More collision.

--- Assets/Scripts/Autofly.cs
+++ Assets/Scripts/Autofly.cs
@@ -21,20 +21,22 @@ public class Autofly : MonoBehaviour {
 			transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
 		}
 
-		/*
+
+
 		Ray ray = Camera.main.ViewportPointToRay (new Vector3 (.5f, .5f, 0));
 		RaycastHit hit;
 
 		if (Physics.Raycast (ray, out hit)) {
-			if (hit.collider.name.Contains (ground.name)) { //"Terrain"
+			/*if (hit.collider.name.Contains (ground.name)) { //"Terrain"
 				if (hit.distance <=5) {
 					flying = false;
 				}
 			} else {
 				flying = true;
-			}
+			}*/
 		} else {
 			flying = true;
-		}*/
+		}
+
 	}
 }
