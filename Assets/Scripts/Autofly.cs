a958255143818e87cc0f3b898e61aaaf4929a0f3
Ladydiana <diana.culincu@gmail.com>
2018-03-21 17:56:16 +0200

    Finished colliders and added aura particle.

--- Assets/Scripts/Autofly.cs
+++ Assets/Scripts/Autofly.cs
@@ -19,6 +19,21 @@ public class Autofly : MonoBehaviour {
 			//transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
 			transform.Translate (Camera.main.transform.forward * speed * Time.deltaTime);
 		}
+
+		Ray ray = Camera.main.ViewportPointToRay (new Vector3 (.5f, .5f, 0));
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit)) {
+			if (hit.collider.tag.Contains ("ArenaDelimiter")) { 
+				if (hit.distance <=5) {
+					flying = false;
+				}
+			} else {
+				flying = true;
+			}
+		} else {
+			flying = true;
+		}
 	}
 
 	void onTriggerEnter(Collider other) {
