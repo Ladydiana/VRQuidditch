b557edb52dd310f53b6d21e4dcfa7da784657463
Ladydiana <diana.culincu@gmail.com>
2018-03-21 14:16:59 +0200

    Collision work.

--- Assets/Scripts/Autofly.cs
+++ Assets/Scripts/Autofly.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
- * Autowalk script
+ * Autoflight script
  * Walks in the direction the player is facing with a given speed.
- * Stop when facing the ground.
  */
 
 public class Autofly : MonoBehaviour {
 
-	public GameObject ground;
 	public float speed = 50.0f;
 	public bool flying = true;
 
@@ -18,25 +16,14 @@ public class Autofly : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (flying) {
-			transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+			//transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+			transform.Translate (Camera.main.transform.forward * speed * Time.deltaTime);
 		}
+	}
 
-
-
-		Ray ray = Camera.main.ViewportPointToRay (new Vector3 (.5f, .5f, 0));
-		RaycastHit hit;
-
-		if (Physics.Raycast (ray, out hit)) {
-			/*if (hit.collider.name.Contains (ground.name)) { //"Terrain"
-				if (hit.distance <=5) {
-					flying = false;
-				}
-			} else {
-				flying = true;
-			}*/
-		} else {
-			flying = true;
+	void onTriggerEnter(Collider other) {
+		if(other.gameObject.tag=="ArenaDelimiter") {
+			Debug.Log ("Arena collision.");
 		}
-
 	}
 }
