941fbd0e58dac602874968e5262fb0fc358557b9
Ladydiana <diana.culincu@gmail.com>
2018-03-27 18:51:54 +0300

    Pickup collisions.

--- Assets/Scripts/Autofly.cs
+++ Assets/Scripts/Autofly.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 /*
  * Autoflight script
- * Walks in the direction the player is facing with a given speed.
+ * Flies in the direction the player is facing with a given speed.
  */
 
 public class Autofly : MonoBehaviour {
@@ -24,26 +24,34 @@ public class Autofly : MonoBehaviour {
 		Ray ray = Camera.main.ViewportPointToRay (new Vector3 (.5f, .5f, 0));
 		RaycastHit hit;
 
+
 		if (Physics.Raycast (ray, out hit)) {
 			if (hit.collider.tag.Contains ("ArenaDelimiter")) { 
-				if (hit.distance <=10) {
+				if (hit.distance <= 16) {
 					Debug.Log ("Hit");
 					//gameObject.GetComponent<Rigidbody> ().velocity = Vector3.Reflect (gameObject.GetComponent<Rigidbody> ().velocity, hit.collider.gameObject.GetComponent<MeshFilter> ().mesh.normals[0]);
 					//Rigidbody.AddForce(otherBall.Rigid_Body.velocity);
+					if (hit.collider.GetComponent<Rigidbody> () != null) {
+						hit.rigidbody.AddForce (Vector3.forward * 1000);
+					}
+					//gameObject.rigidbody.velocity * = -1;
+
 					flying = false;
+					Debug.Log ("flying=false");
+					//gameObject.GetComponent<Rigidbody> ().AddForce (Vector3.back * -8000);
+					Debug.Log ("Knockback");
 					//gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+				} 
+				else {
+					flying = true;
 				}
-			} else {
+			} 
+			else {
 				flying = true;
 			}
-		} else {
+		} 
+		else {
 			flying = true;
-		}
-	}
-
-	void onTriggerEnter(Collider other) {
-		if(other.gameObject.tag=="ArenaDelimiter") {
-			Debug.Log ("Arena collision.");
 		}
 	}
 }
---
+++ Assets/Scripts/PickupLogic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupLogic : MonoBehaviour {
+
+	public int value = 1;
+
+	void OnTriggerEnter (Collider other) {
+		Debug.Log ("Trigger collision detected");
+		if (other.gameObject.tag == "Player") {
+			Debug.Log ("Pickup.");
+			Destroy (gameObject);
+		} 
+		else {
+			// we assume it was spawned somewhere inside the stadium prefab
+			Destroy (gameObject);
+		}
+	}
+
+
+}
