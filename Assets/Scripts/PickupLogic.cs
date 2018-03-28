2f92de7aab66c392f7b146e4870a561532724fd9
Ladydiana <diana.culincu@gmail.com>
2018-03-28 18:54:34 +0300

    Terrain.

--- Assets/Scripts/PickupLogic.cs
+++ Assets/Scripts/PickupLogic.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PickupLogic : MonoBehaviour {
 
 	public int value = 1;
+	public AudioClip destroyAudioSource;
 
 	void OnTriggerEnter (Collider other) {
 		Debug.Log ("Trigger collision detected");
 		if (other.gameObject.tag == "Player") {
 			Debug.Log ("Pickup.");
+
+			if (destroyAudioSource != null) {
+				AudioSource.PlayClipAtPoint (destroyAudioSource, transform.position);
+			}
+
 			Destroy (gameObject);
 		} 
 		else {
