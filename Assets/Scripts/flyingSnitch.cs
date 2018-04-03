06eb1e742963c6e27926039a2571fead413cac90
Ladydiana <diana.culincu@gmail.com>
2018-04-03 17:16:27 +0300

    Working on Snitch Mode.

--- Assets/Scripts/flyingSnitch.cs
+++ Assets/Scripts/flyingSnitch.cs
@@ -5,14 +5,40 @@ using UnityEngine;
 public class flyingSnitch : MonoBehaviour {
 
 	public GameObject[] checkpointPoints;
+	public float snitchSpeed = 10f;
+	private GameManager gameManager;
+	private short lastCheckpoint;
+	private short nextCheckpoint;
 
 	// Use this for initialization
 	void Start () {
-		
+		if (checkpointPoints!=null && checkpointPoints.Length > 0) {
+			transform.position = checkpointPoints [0].transform.position;
+		}
+
+		gameManager = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager>();
+		lastCheckpoint = 0;
+		nextCheckpoint = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (gameManager.gameMode == GameMode.Snitch) {
+			if (transform.position == checkpointPoints [nextCheckpoint].transform.position) {
+				nextCheckpoint++;
+				lastCheckpoint++;
+			}
+
+			if (nextCheckpoint >= checkpointPoints.Length) {
+				nextCheckpoint = 0;
+			}
+
+			if (lastCheckpoint >= checkpointPoints.Length) {
+				lastCheckpoint = 0;
+			}
+
+			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (checkpointPoints [nextCheckpoint].transform.position - transform.position), snitchSpeed * Time.deltaTime);
+			transform.position = Vector3.MoveTowards (transform.position, checkpointPoints [nextCheckpoint].transform.position, snitchSpeed * Time.deltaTime);
+		}
 	}
 }
