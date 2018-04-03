9b375b84cd2067df269b21849961546a09ac7334
Ladydiana <diana.culincu@gmail.com>
2018-04-03 17:31:08 +0300

    Game management stuff for different play modes.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,16 @@ public class GameManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		gameStarted = true;
+
+		// if not in Snitch Mode deactivate Snitch
+		if (gameMode != GameMode.Snitch) {
+			GameObject.FindGameObjectWithTag ("Snitch").SetActive (false);
+		}
+
+		// if not in Hoop mode don't spawn hoops
+		if (gameMode != GameMode.Hoop) {
+			GameObject.FindGameObjectWithTag ("Spawner").SetActive (false);
+		}
 	}
 	
 	// Update is called once per frame
